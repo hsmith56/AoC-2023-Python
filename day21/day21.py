4e01de268e27d93d7f47f0a949809adfdd455ea1
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2023-11-29 08:45:06 -0500

    Goodbye tabs, hello spaces

--- day21/day21.py
+++ day21/day21.py
@@ -1,23 +1,23 @@
 import random, math
 
 def load(example=False):
-	if example is True:
-		with open('day21/day21Example.txt') as file:
-			data = file.readlines()
-			d = [line.strip() for line in data]
-		return d
-		
-	with open('day21/day21Input.txt') as file:
-		data = file.readlines()
-		d = [line.strip() for line in data]
-	return d
+    if example is True:
+        with open('day21/day21Example.txt') as file:
+            data = file.readlines()
+            d = [line.strip() for line in data]
+        return d
+        
+    with open('day21/day21Input.txt') as file:
+        data = file.readlines()
+        d = [line.strip() for line in data]
+    return d
 
 inpt = load()
 
 def part1(data, *args, **kwargs):
-	return data
+    return data
 
 def part2(data, *args, **kwargs):
-	return 0
+    return 0
 
 print(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')
