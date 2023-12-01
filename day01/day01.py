26079441b92788fd408f6c68d72ee2c3fff2ad76
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2023-12-01 08:46:44 -0500

    Day 1 Complete

--- day01/day01.py
+++ day01/day01.py
@@ -12,12 +12,38 @@ def load(example=False):
         d = [line.strip() for line in data]
     return d
 
-inpt = load(example=True)
+inpt = load(example=False)
 
 def part1(data, *args, **kwargs):
-    return data
+    total = 0
+    for entry in data:
+        nums = [x for x in entry if x.isnumeric()]
+        first, last = nums[0], nums[-1]
+        total += int(f"{first}{last}")
+    
+    return total
 
 def part2(data, *args, **kwargs):
-    return 0
+    mapping = { "one": 'one1one',
+                "two": 'two2two',
+                "three":'three3three',
+                "four": 'four4four',
+                "five": 'five5five',
+                "six": 'six6six',
+                "seven":'seven7seven',
+                "eight":'eight8eight',
+                "nine":'nine9nine'
+            }
+
+    total = 0
+    for entry in data:
+        for k,v in mapping.items():
+            entry = entry.replace(k, v)
+        
+        nums = [x for x in entry if x.isnumeric()]
+        first, last = nums[0], nums[-1]
+        total += int(f"{first}{last}")
+    
+    return total
 
 print(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')
